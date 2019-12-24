833c2c05401fbe84529d51dc1eb597c10b6615c9
Jiaming Li <ljm625@gmail.com>
2019-12-24 13:15:07 +0800

    First Commit

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -6,12 +6,16 @@ import random
 
 
 # TODO: Write a print statement that displays both the type and value of `pi`
+
 pi = math.pi
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
+def test(a,x):
 
+
+requests
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
